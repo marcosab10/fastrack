61aed4981a930e14d5f9bc7b1f8bcb7f5a397375
Marcos Antonio Bittencourt Faria <A0096301@redecorp.br>
2022-01-18 12:38:55 -0300

    Hello World

---
+++ ms-order-project/src/main/java/com/fastrack/msorderproject/MsOrderProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fastrack.msorderproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOrderProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsOrderProjectApplication.class, args);
+	}
+
+}
---
+++ ms-order-project/src/test/java/com/fastrack/msorderproject/MsOrderProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastrack.msorderproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsOrderProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
